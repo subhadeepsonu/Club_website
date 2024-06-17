88d05d4b8d62df58c63135c9c3ec167bc34eaf15
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-06-17 13:37:46 +0530

    improved loading state

--- app/Team/[department]/loading.tsx
+++ app/Team/[department]/loading.tsx
@@ -1,5 +1,8 @@
+import DotPattern from "@/components/magicui/dot-pattern";
+
 export default function Loading(){
-    return <div className="h-screen w-full flex justify-center items-center">
+    return <div className="h-screen w-full flex justify-center items-center text-white">
         Loading...
+        <DotPattern></DotPattern>
     </div>
 }
