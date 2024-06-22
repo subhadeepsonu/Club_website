5f6f15ff2e1f7903f89ea1ce05c8bf4d2b55540b
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-06-22 15:08:38 +0530

    k

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,10 +1,13 @@
 import prisma from "@/db";
 import { GetDataFromToken } from "@/utils/GetDataFromTokeen";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req:NextRequest){
     try {
-        const data = await GetDataFromToken(req)
+        const token  = cookies().get('token')
+        const data:any = jwt.decode(token?.value!)
         const response = await prisma.user.findUnique({
             where:{
                 id:data.id
