fc3bf0db30201e8bb34e471e20cb64217da14663
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-06-29 16:48:49 +0530

    chechk

--- utils/mailer.ts
+++ utils/mailer.ts
@@ -18,7 +18,7 @@ export const sendEmail = async (email:string,name:string)=>{
             text:"Sup brother",
             html:`
             <div style="width: 600px; height: 375px; margin: 0 auto; position: relative; background-image: url('https://utfs.io/f/24fcb596-c93f-46c0-9b74-9e571db212e0-22k0zs.45.20_d8065d6d.jpg'); background-size: cover; background-position: center;">
-        <div style=" padding-left: 45%; padding-top: 28%; font-size: x-large; font-weight: bold;">${username[0]}</div>
+        <div style=" padding-left: 35%; padding-top: 28%; font-size: x-large; font-weight: bold;">${username[0]}</div>
     </div>
         `
         })
